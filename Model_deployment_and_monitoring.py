681846eb6fae93265108cb3cf26f43de8f196c7d
Theomurphy <murphys.theo@gmail.com>
2025-07-11 00:07:14 +0100

    Update Model_deployment_and_monitoring.py

--- Model_deployment_and_monitoring.py
+++ Model_deployment_and_monitoring.py
@@ -4,8 +4,8 @@ import pandas as pd
 import joblib
 
 # Importing linear regression trained model
-model = joblib.load("/Model/lr_model.pkl")
-feature_order = joblib.load("/Model/data_features.pkl")
+model = joblib.load("Model/lr_model.pkl")
+feature_order = joblib.load("Model/data_features.pkl")
 
 st.title("🎓 Smart GPA Predictor", width="stretch")
 st.markdown("""
